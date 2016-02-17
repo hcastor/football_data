165d0cc6ec4de86e73a98fd65247ef752262a99f
Henry <hcastor94@gmail.com>
2016-02-16 19:59:31 -0600

    changing tabs to spaces

--- scrapers/getNFLTeamStats.py
+++ scrapers/getNFLTeamStats.py
@@ -8,93 +8,93 @@ from datetime import datetime, timedelta
 
 def main():
 
-	startTime = datetime.now()
-	print startTime
+    startTime = datetime.now()
+    print startTime
 
-	#html5lib parser required for broken html on gameSplits
-	browser = RoboBrowser(history=False, parser='html5lib')
+    #html5lib parser required for broken html on gameSplits
+    browser = RoboBrowser(history=False, parser='html5lib')
 
-	browser.open("http://www.nfl.com/stats/categorystats?tabSeq=2&offensiveStatisticCategory=GAME_STATS&conference=ALL&role=TM&season=2015&seasonType=REG&d-447263-s=TOTAL_YARDS_GAME_AVG&d-447263-o=2&d-447263-n=1")
+    browser.open("http://www.nfl.com/stats/categorystats?tabSeq=2&offensiveStatisticCategory=GAME_STATS&conference=ALL&role=TM&season=2015&seasonType=REG&d-447263-s=TOTAL_YARDS_GAME_AVG&d-447263-o=2&d-447263-n=1")
 
-	#get form and form options
-	form = browser.get_form(action="/stats/categorystats")
-	role = browser.find(id="role")
-	roles = role.find_all("option")
-	offensiveCategory = browser.find(id="offensive-category")
-	offensiveCategories = offensiveCategory.find_all("option")
-	defensiveCategory = browser.find(id="defensive-category")
-	defensiveCategories = defensiveCategory.find_all("option")
-	season = browser.find(id="season-dropdown")
-	seasons = season.find_all("option")
-	seasonType = browser.find(id="season-type")
-	seasonTypes = seasonType.find_all("option")
+    #get form and form options
+    form = browser.get_form(action="/stats/categorystats")
+    role = browser.find(id="role")
+    roles = role.find_all("option")
+    offensiveCategory = browser.find(id="offensive-category")
+    offensiveCategories = offensiveCategory.find_all("option")
+    defensiveCategory = browser.find(id="defensive-category")
+    defensiveCategories = defensiveCategory.find_all("option")
+    season = browser.find(id="season-dropdown")
+    seasons = season.find_all("option")
+    seasonType = browser.find(id="season-type")
+    seasonTypes = seasonType.find_all("option")
 
-	teamStatDict = defaultdict(dict)
-	for role in roles:
-		print role['value']
-		availableCategories = None
-		if role.text == "Offense":
-			availableCategories = offensiveCategories
-		elif role.text == "Defense":
-			availableCategories = defensiveCategories
-		else:
-			print "unknown role"
+    teamStatDict = defaultdict(dict)
+    for role in roles:
+        print role['value']
+        availableCategories = None
+        if role.text == "Offense":
+            availableCategories = offensiveCategories
+        elif role.text == "Defense":
+            availableCategories = defensiveCategories
+        else:
+            print "unknown role"
 
-		for category in availableCategories:
-			if category.text == "Category...":
-				continue
-			print category['value']
-			for season in seasons:
-				if season.text == "Season...":
-					continue
-				print season['value']
-				for seasonType in seasonTypes:
-					if seasonType.text == "Season Type...":
-						continue
-					print seasonType['value']
-					form["role"].value = role['value']
-					if role.text == "Offense":
-						form['offensiveStatisticCategory'].value = category['value']
-					elif role.text == "Defense":
-						form['defensiveStatisticCategory'].value = category['value']
-					form['season'].value = season['value']
-					form['seasonType'].value = seasonType['value']
-					browser.submit_form(form)
-					result = browser.find(id="result")
-					if result:
-						tbodies = result.find_all("tbody")
-						if len(tbodies) != 2:
-							print "error parsing result"
-						tableKey = tbodies[0]
-						tableKey = tableKey.find_all("th")
+        for category in availableCategories:
+            if category.text == "Category...":
+                continue
+            print category['value']
+            for season in seasons:
+                if season.text == "Season...":
+                    continue
+                print season['value']
+                for seasonType in seasonTypes:
+                    if seasonType.text == "Season Type...":
+                        continue
+                    print seasonType['value']
+                    form["role"].value = role['value']
+                    if role.text == "Offense":
+                        form['offensiveStatisticCategory'].value = category['value']
+                    elif role.text == "Defense":
+                        form['defensiveStatisticCategory'].value = category['value']
+                    form['season'].value = season['value']
+                    form['seasonType'].value = seasonType['value']
+                    browser.submit_form(form)
+                    result = browser.find(id="result")
+                    if result:
+                        tbodies = result.find_all("tbody")
+                        if len(tbodies) != 2:
+                            print "error parsing result"
+                        tableKey = tbodies[0]
+                        tableKey = tableKey.find_all("th")
 
-						tableItems = tbodies[1]
-						tableItems = tableItems.find_all("td")
+                        tableItems = tbodies[1]
+                        tableItems = tableItems.find_all("td")
 
-						team = None
-						tableColumn = 0
-						for tableItem in tableItems:
-							if tableColumn == 0:
-								tableColumn += 1
-								continue
+                        team = None
+                        tableColumn = 0
+                        for tableItem in tableItems:
+                            if tableColumn == 0:
+                                tableColumn += 1
+                                continue
 
-							if tableColumn == 1:
-								team = tableItem.text
-								tableColumn += 1
-								continue
+                            if tableColumn == 1:
+                                team = tableItem.text
+                                tableColumn += 1
+                                continue
 
-							teamStatHashTuple = (re.sub('[\t,\n,\r]', '', team), re.sub('[\t,\n,\r]', '', season.text), re.sub('[\t,\n,\r]', '', seasonType.text), re.sub('[\t,\n,\r]', '', role.text))
-							teamStatDict[teamStatHashTuple][re.sub('[\t,\n,\r]', '', tableKey[tableColumn].text)] = re.sub('[\t,\n,\r]', '', tableItem.text)
+                            teamStatHashTuple = (re.sub('[\t,\n,\r]', '', team), re.sub('[\t,\n,\r]', '', season.text), re.sub('[\t,\n,\r]', '', seasonType.text), re.sub('[\t,\n,\r]', '', role.text))
+                            teamStatDict[teamStatHashTuple][re.sub('[\t,\n,\r]', '', tableKey[tableColumn].text)] = re.sub('[\t,\n,\r]', '', tableItem.text)
 
-							tableColumn += 1
-							if tableColumn >= len(tableKey):
-								tableColumn = 0
-					print teamStatDict
-					raw_input('-->')
-	for keyTuple, categories in teamStatDict.iteritems():
-		print keyTuple
-		print categories
-	print datetime.now()-startTime 
+                            tableColumn += 1
+                            if tableColumn >= len(tableKey):
+                                tableColumn = 0
+                    print teamStatDict
+                    raw_input('-->')
+    for keyTuple, categories in teamStatDict.iteritems():
+        print keyTuple
+        print categories
+    print datetime.now()-startTime 
 
 if __name__ == '__main__':
-	main()
+    main()
--- scrapers/getRotoFDStats.py
+++ scrapers/getRotoFDStats.py
@@ -7,39 +7,39 @@ from datetime import datetime, timedelta
 
 def main():
 
-	startTime = datetime.now()
-	print startTime
+    startTime = datetime.now()
+    print startTime
 
-	browser = RoboBrowser(history=False,  parser='html.parser')
+    browser = RoboBrowser(history=False,  parser='html.parser')
 
-	pages = [(2011, 17), (2012, 17), (2013, 17), (2014, 17), (2015, 17)]
-		
-	headers = []
-	dataList = []
-	for year, maxWeek in pages:
-		for week in range(1, maxWeek+1):
-			browser.open("http://rotoguru1.com/cgi-bin/fyday.pl?week={}&year={}&game=fd&scsv=1".format(week, year))
+    pages = [(2011, 17), (2012, 17), (2013, 17), (2014, 17), (2015, 17)]
+        
+    headers = []
+    dataList = []
+    for year, maxWeek in pages:
+        for week in range(1, maxWeek+1):
+            browser.open("http://rotoguru1.com/cgi-bin/fyday.pl?week={}&year={}&game=fd&scsv=1".format(week, year))
 
-			data = browser.find('pre')
-			headers.append(data.text[:56].replace(';', ','))
-			dataList.append(data.text[57:].replace(';', ','))
+            data = browser.find('pre')
+            headers.append(data.text[:56].replace(';', ','))
+            dataList.append(data.text[57:].replace(';', ','))
 
-	if any(x != 'Week;Year;GID;Name;Pos;Team;h/a;Oppt;FD points;FD salary'.replace(';', ',') for x in headers):
-		print 'HEADER ERROR'
-		return -1
-	else:
-		print "perfect"
+    if any(x != 'Week;Year;GID;Name;Pos;Team;h/a;Oppt;FD points;FD salary'.replace(';', ',') for x in headers):
+        print 'HEADER ERROR'
+        return -1
+    else:
+        print "perfect"
 
-	outputFile = open(r"./rotoFDStats.csv", 'w')
-	outputFile.write(headers[0] + '\n')
-	outputFile.close()
+    outputFile = open(r"./rotoFDStats.csv", 'w')
+    outputFile.write(headers[0] + '\n')
+    outputFile.close()
 
-	for data in dataList:
-		with open(r"./rotoFDStats.csv", 'a+') as outputFile:
-			outputFile.write(data)
+    for data in dataList:
+        with open(r"./rotoFDStats.csv", 'a+') as outputFile:
+            outputFile.write(data)
 
 
-	print datetime.now()-startTime 
+    print datetime.now()-startTime 
 
 if __name__ == '__main__':
-	main()
+    main()
