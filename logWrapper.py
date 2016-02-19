ca2e72b14833cb733dc0be2f690bb377606193f0
Henry <hcastor94@gmail.com>
2016-02-18 21:14:16 -0600

    allowing multiple calls to makeLogger

--- logWrapper.py
+++ logWrapper.py
@@ -8,20 +8,21 @@ def makeLogger(loggerName, logDir):
 
     loggerName = str(loggerName)
     logger = logging.getLogger(loggerName)
-    logger.setLevel(logging.DEBUG)
-    timeStamp = datetime.now().strftime("_%Y-%m-%d_%H-%M-%S")
-    debug_file_name = logDir + 'debug_' + loggerName + timeStamp + '.log'
-    error_file_name = logDir + 'error_'+ loggerName + timeStamp +'.log'
-    dfh = logging.FileHandler(debug_file_name)
-    dfh.setLevel(logging.DEBUG)
-    efh = logging.FileHandler(error_file_name)
-    efh.setLevel(logging.ERROR)
-    formatter = logging.Formatter('%(asctime)s |~| %(processName)s |~| %(name)s |~| %(levelname)s |~| %(message)s')
-    efh.setFormatter(formatter)
-    dfh.setFormatter(formatter)
-    # add the handlers to logger
-    logger.addHandler(efh)
-    logger.addHandler(dfh)
+    if len(logger.handlers) == 0:
+        logger.setLevel(logging.DEBUG)
+        timeStamp = datetime.now().strftime("_%Y-%m-%d_%H-%M-%S")
+        debug_file_name = logDir + 'debug_' + loggerName + timeStamp + '.log'
+        error_file_name = logDir + 'error_'+ loggerName + timeStamp +'.log'
+        dfh = logging.FileHandler(debug_file_name)
+        dfh.setLevel(logging.DEBUG)
+        efh = logging.FileHandler(error_file_name)
+        efh.setLevel(logging.ERROR)
+        formatter = logging.Formatter('%(asctime)s |~| %(processName)s |~| %(name)s |~| %(levelname)s |~| %(message)s')
+        efh.setFormatter(formatter)
+        dfh.setFormatter(formatter)
+        # add the handlers to logger
+        logger.addHandler(efh)
+        logger.addHandler(dfh)
     return logger
 
 def closeLogger(loggerName):
