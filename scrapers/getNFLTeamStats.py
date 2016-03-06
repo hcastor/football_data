435368423bb04d56aa76a58f14ae56ec29233e4a
Henry <hcastor94@gmail.com>
2016-03-06 14:05:06 -0600

    Changing multiprocces level, and adding better error handling

--- scrapers/getNFLTeamStats.py
+++ scrapers/getNFLTeamStats.py
@@ -22,37 +22,35 @@ col_nfl_team_stats = db['nfl_team_stats']
 def removeNewLine(value):
     return re.sub('[\t,\n,\r]', '', value)
 
-def parseCategory(role, category, seasons, seasonTypes):
-    logger = makeLogger(role.text + '_' + category.text, r'./logs_nflteamStat/')
+def parseSeason(role, category, season, seasonTypes):
+    logger = makeLogger(role.text + '_' + category.text + '_' + season.text, r'./logs_nflteamStat/')
 
     startTime = datetime.now()
 
-    logger.debug('Starting %s %s', role.text, category.text)
+    logger.debug('Starting %s %s %s', role.text, category.text, season.text)
 
     teamStat_list = []
-    for season in seasons:
-        if season.text == "Season..." or convertToNumber(removeNewLine(season.text)) < 1960:
+    for seasonType in seasonTypes:
+        if seasonType.text == "Season Type...":
             continue
-        for seasonType in seasonTypes:
-            if seasonType.text == "Season Type...":
-                continue
 
-            team_stats_query = {'year': convertToNumber(removeNewLine(season.text)),
-                'seasonType': removeNewLine(seasonType.text),
-                'role': removeNewLine(role.text),
-                'category': removeNewLine(category.text)
-            }
+        team_stats_query = {'year': convertToNumber(removeNewLine(season.text)),
+            'seasonType': removeNewLine(seasonType.text),
+            'role': removeNewLine(role.text),
+            'category': removeNewLine(category.text)
+        }
 
-            if col_nfl_team_stats.find(team_stats_query).count():
-                logger.debug('Already parsed %s %s', role.text, category.text)
-                return None
+        if col_nfl_team_stats.find(team_stats_query).count():
+            logger.debug('Already parsed %s', team_stats_query)
+            continue
 
-            wait = random.uniform(1.5,3.5)
-            logger.debug('Waiting %f', wait)
-            time.sleep(wait)
+        wait = random.uniform(1.5,3.5)
+        logger.debug('Waiting %f', wait)
+        time.sleep(wait)
 
-            logger.debug('Starting: %s', team_stats_query)
-            url = 'http://www.nfl.com/stats/categorystats?' + 'archive=true&conference=null' + '&role=' + role['value']
+        logger.debug('Starting: %s', team_stats_query)
+        url = 'http://www.nfl.com/stats/categorystats?' + 'archive=true&conference=null' + '&role=' + role['value']
+        try:
             if role.text == "Offense":
                 categoryUrl = '&offensiveStatisticCategory=' + category['value'] + '&defensiveStatisticCategory=null'
                 
@@ -66,65 +64,68 @@ def parseCategory(role, category, seasons, seasonTypes):
 
             logger.debug('Opening: %s', url)
             browser = RoboBrowser(history=False,  parser='html5lib', user_agent=get_user_agent(logger), timeout=10)
-            result = None
+            browser = open_or_follow_link(logger, browser, 'open', url)
+            result = browser.find(id="result")
+
             tries = 0
-            while(not result):
+            # sometimes when using slow proxies nfl.com returns 200 without the whole page being loaded
+            while not result:
+                if tries > 10:
+                    raise Exception('No teams found %s' % url)
+                elif tries > 0:
+                    time.sleep(random.uniform(5, 7))
                 tries += 1
-                logger.debug('tries: %d', tries)
+                logger.debug('No result-tries: %d', tries)
+                browser = RoboBrowser(history=False,  parser='html5lib', user_agent=get_user_agent(logger), timeout=10)
                 browser = open_or_follow_link(logger, browser, 'open', url)
                 result = browser.find(id="result")
-                if tries > 10:
-                    raise exception('10 tries in a row %s failed', url)
-                else:
-                    time.sleep(random.uniform(.5, 2.5))
 
+            tbodies = result.find_all("tbody")
+            if len(tbodies) != 2:
+                raise Exception("error parsing result")
+            tableKey = tbodies[0]
+            tableKey = tableKey.find_all("th")
 
-            try:
-                tbodies = result.find_all("tbody")
-                if len(tbodies) != 2:
-                    raise Exception("error parsing result")
-                tableKey = tbodies[0]
-                tableKey = tableKey.find_all("th")
+            tableItems = tbodies[1]
+            tableItems = tableItems.find_all("td")
 
-                tableItems = tbodies[1]
-                tableItems = tableItems.find_all("td")
-
-                tableColumn = 0
-                teamStatDict = {}
-                for tableIndex, tableItem in enumerate(tableItems):
-                    if tableColumn == 0:
-                        logger.debug('Row %d of %d', tableIndex, len(tableItems))
-                        tableColumn += 1
-                        continue
-
-                    if tableColumn == 1:
-                        teamStatDict['team'] = removeNewLine(tableItem.text)
-                        teamStatDict['year'] = convertToNumber(removeNewLine(season.text))
-                        teamStatDict['seasonType'] = removeNewLine(seasonType.text)
-                        teamStatDict['role'] = removeNewLine(role.text)
-                        teamStatDict['category'] = removeNewLine(category.text)
-                        tableColumn += 1
-                        continue
-
-                    key = cleanKey(removeNewLine(tableKey[tableColumn].text))
-                    value = convertToNumber(removeNewLine(tableItem.text))
-                    teamStatDict[key] = value
-
+            tableColumn = 0
+            teamStatDict = {}
+            for tableIndex, tableItem in enumerate(tableItems):
+                if tableColumn == 0:
+                    logger.debug('Row %d of %d', tableIndex, len(tableItems))
                     tableColumn += 1
-                    if tableColumn >= len(tableKey):
-                        teamStat_list.append(teamStatDict)
-                        teamStatDict = {}
-                        tableColumn = 0
-            except:
-                logger.exception('row fail: tableColumn: %d tableKeyLen: %d tableItemLen: %d', tableColumn, len(tableKey), len(tableItems))
+                    continue
+
+                if tableColumn == 1:
+                    teamStatDict['team'] = removeNewLine(tableItem.text)
+                    teamStatDict['year'] = convertToNumber(removeNewLine(season.text))
+                    teamStatDict['seasonType'] = removeNewLine(seasonType.text)
+                    teamStatDict['role'] = removeNewLine(role.text)
+                    teamStatDict['category'] = removeNewLine(category.text)
+                    tableColumn += 1
+                    continue
+
+                key = cleanKey(removeNewLine(tableKey[tableColumn].text))
+                value = convertToNumber(removeNewLine(tableItem.text))
+                teamStatDict[key] = value
+
+                tableColumn += 1
+                if tableColumn >= len(tableKey):
+                    teamStat_list.append(teamStatDict)
+                    teamStatDict = {}
+                    tableColumn = 0
+        except:
+            logger.exception('row fail')
 
     try:
-        logger.debug('Bulk Creating teamStat_list')
-        col_nfl_team_stats.insert_many(teamStat_list)
+        if teamStat_list:
+            logger.debug('Bulk Creating teamStat_list')
+            col_nfl_team_stats.insert_many(teamStat_list)
     except:
         logger.exception('insert_many error')
 
-    logger.debug('parseCategory time elapsed: ' + str(datetime.now() - startTime))
+    logger.debug('parseSeason time elapsed: ' + str(datetime.now() - startTime))
 
     closeLogger(role.text + '_' + category.text)
 
@@ -141,9 +142,8 @@ def main():
     browser = RoboBrowser(history=False,  parser='html5lib', user_agent=get_user_agent(logger), timeout=10)
     startingUrl = "http://www.nfl.com/stats/categorystats?tabSeq=2&offensiveStatisticCategory=GAME_STATS&conference=ALL&role=TM&season=2015&seasonType=REG"
     browser = open_or_follow_link(logger, browser, 'open', startingUrl)
-
-    role = browser.find(id="role")
     
+    role = browser.find(id="role")
     roles = role.find_all("option")
     offensiveCategory = browser.find(id="offensive-category")
     offensiveCategories = offensiveCategory.find_all("option")
@@ -167,8 +167,11 @@ def main():
         for category in availableCategories:
             if category.text == "Category...":
                 continue
-            parseCategory(role, category, seasons, seasonTypes)
-            #pool.apply_async(parseCategory, (role, category, seasons, seasonTypes,))
+            for season in seasons:
+                if season.text == "Season..." or convertToNumber(removeNewLine(season.text)) < 1960:
+                    continue
+                #parseSeason(role, category, season, seasonTypes)
+                pool.apply_async(parseSeason, (role, category, season, seasonTypes,))
 
     pool.close() #Prevents any more tasks from being submitted to the pool. Once all the tasks have been completed the worker processes will exit.
     pool.join() #Wait for the worker processes to exit. One must call close() or terminate() before using join().
